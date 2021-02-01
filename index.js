09b24fcffa5b903ef51c348e77b0c4b8a90ea5a6
Юля <46893382+R-Yuliya@users.noreply.github.com>
2021-02-01 22:10:30 +1000

    Главный файл. Оптимизация кода.
    Спасибо, zziger <3

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 
     Если у Вас есть предложения по улучшению данной шняги - пишите мне в телеграм: @zefif
 */
-
 const easyvk = require('easyvk');
-const { Client, MessageEmbed } = require('discord.js');
+const {
+    Client,
+    MessageEmbed
+} = require('discord.js');
 const superagent = require('superagent')
 const client = new Client();
 
@@ -22,127 +24,109 @@ easyvk({
     password: cfg.password // пароль
 }).then(async vk => {
 
-  // функция для получения полной информации о сообщении
-  async function getMessage (msgArray = []) {
-    const MESSAGE_ID__INDEX = 1;
-  
-    return vk.call('messages.getById', {
-      message_ids: msgArray[MESSAGE_ID__INDEX]
-    })
-  }
+    // функция для получения полной информации о сообщении
+    async function getMessage(msgArray = []) {
+        const MESSAGE_ID__INDEX = 1;
 
-  client.on('ready', async() => {
-    console.log('booted')
-  })
-
-  client.on('message', async(message) => {
-
-    //отправка сообщений в чаты
-    if(!message.author.bot && message.channel.parentID == cfg.parent_id) {
-      vk.call('messages.send', {
-        user_id: `${message.channel.topic}`,
-        message: message.content,
-        random_id: easyvk.randomId()
-      })
-      message.delete()
+        return vk.call('messages.getById', {
+            message_ids: msgArray[MESSAGE_ID__INDEX]
+        })
     }
-  })
+
+    client.on('ready', async () => {
+        console.log('booted')
+    })
+
+    client.on('message', async (message) => {
+
+        //отправка сообщений в чаты
+        if (!message.author.bot && message.channel.parentID == cfg.parent_id) {
+            vk.call('messages.send', {
+                user_id: `${message.channel.topic}`,
+                message: message.content,
+                random_id: easyvk.randomId()
+            })
+            message.delete()
+        }
+    })
 
 
     const lpSettings = {
         forGetLongPollServer: {
-          lp_version: 3,
-          need_pts: 1
+            lp_version: 3,
+            need_pts: 1
         },
         forLongPollServer: {
-          wait: 15
+            wait: 15
         }
-      }
-    
+    }
+
     // коннектим longpoll
     vk.longpoll.connect(lpSettings).then(async lp => {
 
-        lp.on('message', async(message) => {
+        lp.on('message', async (message) => {
 
-          const guild = client.guilds.cache.get(cfg.guild_id);
+            const guild = client.guilds.cache.get(cfg.guild_id);
 
-          let fullMessage = await getMessage(message);
-          fullMessage = fullMessage.items[0];
+            let fullMessage = await getMessage(message);
+            fullMessage = fullMessage.items[0];
 
-          // debug
-          console.log(fullMessage);
+            // debug
+            console.log(fullMessage);
 
-          // запрос для получения информации о пользователе, который отправил сообщение
-          let { body } = await superagent.get(`https://api.vk.com/method/users.get?user_ids=${fullMessage.from_id}&fields=photo_max&access_token=${vk.session.access_token}&v=5.126`);
+            // запрос для получения информации о пользователе, который отправил сообщение
+            let {body} = await superagent.get(`https://api.vk.com/method/users.get?user_ids=${fullMessage.from_id}&fields=photo_max&access_token=${vk.session.access_token}&v=5.126`);
 
-          let attachmentsArray;
-          let content = message[5];
+            let attachmentsArray;
+            let content = message[5];
 
-          if(fullMessage.attachments[0]) {
-            if(fullMessage.attachments[0].type == 'photo') {
+            for (const attachment of fullMessage.attachments)
+                switch (attachment.type) {
+                    case 'photo':
+                        content = content || 'Картинка';
+                        attachmentsArray.push(...attachment.photo.sizes);
+                        break;
+                    case 'sticker':
+                        content = content || 'Стикер';
+                        attachmentsArray.push(...attachment.sticker.images);
+                        break;
+                    case 'wall':
+                        content += `\n[Запись на стене](https://vk.com/feed?w=wall${attachment.wall.from_id}_${attachment.wall.id})`;
+                        attachmentsArray.push(...wall.attachments);
+                        break;
+                    case 'audio':
+                        content += '\nАудио';
+                        break;
+                }
 
-              content = 'Картинка';
+            // ищем канал, описание которого содержит ID пользователя. Если не находим - создаём и задаём ID в описании
+            let channel = guild.channels.cache.find(channel => channel.topic == `${message[3]}`);
 
-              if(message[5].length > 0) {
-                content = message[5]
-              }
+            // ищем канал, описание которого содержит ID пользователя. Если не находим - создаём и задаём ID в описании
+            if (!channel) {
+                channel = await guild.channels.create(`${body.response[0].first_name} ${body.response[0].last_name}`, {
+                    reason: 'Канала не существовало',
+                    type: 'text',
+                    parent: cfg.parent_id
+                });
+                await channel.setTopic(body.response[0].id);
+            }
 
-            attachmentsArray = fullMessage.attachments[0].photo.sizes;
+            let msg = new MessageEmbed()
+                .setTitle(`Сообщение`)
+                .setThumbnail(body.response[0].photo_max)
+                .setColor('#7289DA')
+                .addFields({
+                    name: `${body.response[0].first_name} ${body.response[0].last_name}`,
+                    value: content.replace('<br>', '\n')
+                })
+                .setTimestamp()
+            if (attachmentsArray) msg.setImage(attachmentsArray[attachmentsArray.length - 1].url)
 
-             }
-            if(fullMessage.attachments[0].type == 'sticker') { 
-              attachmentsArray = fullMessage.attachments[0].sticker.images
-              content = 'Стикер';
-             }
-            if(fullMessage.attachments[0].type == 'wall') { 
-              attachmentsArray = fullMessage.attachments[0].wall.attachments
-              content = `[Запись на стене](https://vk.com/feed?w=wall${fullMessage.attachments[0].wall.from_id}_${fullMessage.attachments[0].wall.id})`;
-             }
+            // отправляем туда полученное сообщение
+            channel.send(msg)
+        })
+    })
 
-             if(fullMessage.attachments[0].type == 'audio') {
-                 content = 'Аудио'
-             }
-          }
-
-          // ищем канал, описание которого содержит ID пользователя. Если не находим - создаём и задаём ID в описании
-          if(!guild.channels.cache.find(channel => channel.topic == `${message[3]}`)) {
-            guild.channels.create(`${body.response[0].first_name} ${body.response[0].last_name}`, { reason: 'Канала не существовало', type: 'text', parent: cfg.parent_id}).then(async channel => {
-              channel.setTopic(body.response[0].id);
-              
-              // сразу же отправляем в новый канал полученное сообщение
-              let msg = new MessageEmbed()
-              .setTitle(`Сообщение`)
-              .setThumbnail(body.response[0].photo_max)
-              .setColor('#7289DA')
-              .addFields({
-                name: `${body.response[0].first_name} ${body.response[0].last_name}`,
-                value: content.replace('<br>', '\n')
-              })
-              .setTimestamp()
-              if(attachmentsArray) msg.setImage(attachmentsArray[attachmentsArray.length - 1].url)
-                
-              channel.send(msg)
-            })
-          }
-        
-          // ищем канал, описание которого содержит ID пользователя
-          const direct = guild.channels.cache.find(channel => channel.topic == `${message[3]}`)
-
-          let msg = new MessageEmbed()
-            .setTitle(`Сообщение`)
-            .setThumbnail(body.response[0].photo_max)
-            .setColor('#7289DA')
-            .addFields({
-              name: `${body.response[0].first_name} ${body.response[0].last_name}`,
-              value: content.replace('<br>', '\n')
-            })
-            .setTimestamp()
-            if(attachmentsArray) msg.setImage(attachmentsArray[attachmentsArray.length - 1].url)
-
-          // отправляем туда полученное сообщение
-          direct.send(msg)
-      })
-  })
-
-  client.login(cfg.bot_token);
+    client.login(cfg.bot_token);
 })
