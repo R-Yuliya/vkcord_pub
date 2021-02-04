14fa327d494435d5d5f9666cdd0ed3733b7b7bae
Юля <46893382+R-Yuliya@users.noreply.github.com>
2021-02-04 21:13:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -77,28 +77,38 @@ easyvk({
             // запрос для получения информации о пользователе, который отправил сообщение
             let {body} = await superagent.get(`https://api.vk.com/method/users.get?user_ids=${fullMessage.from_id}&fields=photo_max&access_token=${vk.session.access_token}&v=5.126`);
 
-            let attachmentsArray = [];
-            let content = message[5];
-
+            let attachmentsArray;
+            let content = {
+                title: "",
+                text: message[5]
+              };
             for (const attachment of fullMessage.attachments)
                 switch (attachment.type) {
                     case 'photo':
-                        content = content || 'Картинка';
-                        attachmentsArray.push(...attachment.photo.sizes);
+                        content.title = 'Фотография';
+                        content.text = content.text || '\u200B';
+                        attachmentsArray = fullMessage.attachments[0].photo.sizes;
                         break;
                     case 'sticker':
-                        content = content || 'Стикер';
-                        attachmentsArray.push(...attachment.sticker.images);
+                        content.title = 'Стикер';
+                        attachmentsArray = fullMessage.attachments[0].sticker.images;
                         break;
                     case 'wall':
-                        content += `\n[Запись на стене](https://vk.com/feed?w=wall${attachment.wall.from_id}_${attachment.wall.id})`;
-                        attachmentsArray.push(...wall.attachments);
+                        content.title = 'Запись на стене';
+                        content.text += `\n[Перейти к записи](https://vk.com/feed?w=wall${attachment.wall.from_id}_${attachment.wall.id})`;
                         break;
                     case 'audio_message':
-                        content += '\nГолосовое сообщение';
+                        content.title += 'Голосовое сообщение';
                         break;
                     case 'audio':
-                        content += `\nАудиозапись\nНазвание: ${attachment.audio.artist} - ${attachment.audio.title}`;
+                        content.title = 'Аудиозапись'
+                        content.text += `Название: ${attachment.audio.artist} - ${attachment.audio.title}`;
+                        break;
+                    case 'video':
+                        content.title += 'Видео';
+                        break;
+                    case 'doc':
+                        content.title += 'Документ'
                         break;
                 }
 
@@ -116,15 +126,15 @@ easyvk({
             }
 
             let msg = new MessageEmbed()
-                .setTitle(`Сообщение`)
+                .setTitle(content.title)
                 .setThumbnail(body.response[0].photo_max)
                 .setColor('#7289DA')
                 .addFields({
                     name: `${body.response[0].first_name} ${body.response[0].last_name}`,
-                    value: content.replace('<br>', '\n')
+                    value: content.text.replace('<br>', '\n') || '\u200B'
                 })
                 .setTimestamp()
-            if (attachmentsArray > 1) msg.setImage(attachmentsArray[attachmentsArray.length - 1].url)
+            if(attachmentsArray) msg.setImage(attachmentsArray[attachmentsArray.length - 1].url)
 
             // отправляем туда полученное сообщение
             channel.send(msg)
